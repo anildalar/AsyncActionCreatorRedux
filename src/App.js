f0f1ff35e532a34c7da2bd4ecd36405d5fbd65ea
“anildalar” <dollorinfotech@gmail.com>
2022-04-21 09:36:17 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeNameAsyncActionCreator } from '.';
 
 function App() {
+  //1. State/Hook
+  let state = useSelector(store => store); // getState()
+
+  let dispatch = useDispatch();   //dispatch()
+
+  //2. Function defination
+
+
+  //3. Return Statement
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {console.log(state.fullname)}
+        <h1>{state.fullname}</h1>
+        <button onClick={(e)=>{ dispatch(changeNameAsyncActionCreator()) }}>Change My Name</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
+
+let initialState = {
+    fullname:'Anil DOLLOR'
+}
+let rootReducer = (prevState,action)=>{
+  var newState = prevState;
+  switch(action.type){
+    case 'CHANGE_TO_RITIK':
+      return {
+        ...newState,
+        fullname: 'RITIK PATIDAR'
+      }
+    case 'CHANGE_TO_ABHISHEK':
+      return {
+        ...newState,
+        fullname: 'ABHISHEK SUTHAR'
+      }
+    default:
+      return {
+        ...newState,
+        //fullname: 'ABHISHEK SUTHAR'
+      }
+  }
+  
+}
+
+export const ChangeNameActionCreator = ()=>{ //Action Creator
+  return {
+    type:'CHANGE_TO_RITIK'
+  }
+}
+
+export const changeNameAsyncActionCreator = ()=>{ // Async Action Creatore
+  return function(dispatch){
+      //API CALLINg
+      //OR
+
+      //
+
+      setTimeout(function(){
+        dispatch(ChangeNameActionCreator());
+      },5000);
+        
+  }
+}
+let store = createStore(rootReducer,initialState,applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
